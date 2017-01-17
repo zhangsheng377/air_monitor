83c5b3c7e92f58781d569aaf764f949daab6d11d
张胜东 <435878393@qq.com>
2017-01-17 20:20:27 +0800

    添加了自动识别域名为ip的功能，添加了json库，封装了使用接口

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,118 +1,44 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+ï»¿#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <Winsock2.h>
 #include <string>
+#include <iostream>
+
+#include "json\json.hpp"
+#include "yeelink_read\yeelink_read.hpp"
 
 
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
-
-//Ô¶³ÌIP£¬port£¬ÒªÏÂÔØµÄÎÄ¼þµÄ¾ø¶ÔÂ·¾¶£¬±¾µØ±£´æµÄ¾ø¶ÔÂ·¾¶ 
-bool  GetHTTP(string sRemoteIP, u_short nRemotePort, string sRemoteFilePath, string sSavePath)
-{
-	try 
-	{
-		WORD wVersionRequested  = MAKEWORD(1,1);
-	WSADATA wsaData;
-	int nRet;
-	nRet  = WSAStartup(wVersionRequested, &wsaData);
-	if (nRet)
-	{
-		char ermsg[1024] = { 0 };
-		sprintf_s(ermsg,"download %s,WSAStartup() error,error code : %d",sRemoteFilePath, nRet);
-		throw ermsg;
-	}
-
-	if (wsaData.wVersion  != wVersionRequested)
-		throw "WinSock version not supported";
-
-	SOCKET Socket;
-
-	Socket  = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (Socket  == INVALID_SOCKET)
-	{
-		char ermsg[1024] = { 0 };
-		sprintf_s(ermsg,"download %s,socket() error,error code : %d", sRemoteFilePath,WSAGetLastError());
-
-		throw ermsg;
-	}
-
-	// Find the port number for the HTTP service on TCP 
-	SOCKADDR_IN saServer;
-	saServer.sin_port  = htons(nRemotePort);
-	saServer.sin_family  = AF_INET;
-	saServer.sin_addr.s_addr  = inet_addr(sRemoteIP.c_str());
-
-	nRet  = connect(Socket, (LPSOCKADDR)&saServer, sizeof(SOCKADDR_IN));
-	if (nRet  == SOCKET_ERROR)
-	{
-		char ermsg[1024] = { 0 };
-		sprintf_s(ermsg,"download %s,connect() error,error code : %d",sRemoteFilePath, WSAGetLastError());
-		closesocket(Socket);
-		throw ermsg;
-	}
-
-	// Format the HTTP request 
-	char szBuffer[102400];
-
-	sprintf_s(szBuffer, "GET %s\n", sRemoteFilePath.c_str());
-	nRet  = send(Socket, szBuffer, strlen(szBuffer), 0);
-	if (nRet  == SOCKET_ERROR)
-	{
-		char ermsg[1024] = { 0 };
-		sprintf_s(ermsg,"download %s,send() error,error code : %d", sRemoteFilePath,WSAGetLastError());
-		closesocket(Socket);
-		throw ermsg;
-	}
-
-	FILE *fp ;
-	int nError  = fopen_s(&fp,sSavePath.c_str(),"wb");
-	if (nError)
-	{
-		char ermsg[10240] = { 0 };
-		sprintf_s(ermsg,"save %s file error.", sSavePath.c_str());
-		closesocket(Socket);
-		throw ermsg;
-	}
-
-	while (1)
-	{
-		nRet  = recv(Socket, szBuffer, sizeof(szBuffer), 0);
-		if (nRet  == SOCKET_ERROR)
-		{
-			char ermsg[1024] = { 0 };
-			sprintf_s(ermsg,"download %s,recv() error,error code : %d",sRemoteFilePath, WSAGetLastError());
-			closesocket(Socket);
-			throw ermsg;
-		}
-
-		if (nRet  == 0)
-			break;
-
-		fwrite(szBuffer, nRet, 1, fp);
-	}
-	fclose(fp);
-	closesocket(Socket);
-	WSACleanup();
-	}
-		catch (char* sErrMsg)
-	{
-			printf("error\n");
-
-		WSACleanup();
-		return false;
-	}
-
-	return true;
-}
+using json = nlohmann::json;
 
 
-void main()
+
+
+
+
+
+int main()
 {
 	//GetHTTP("42.96.164.52", 80, "http://api.yeelink.net/v1.0/device/353097/sensor/397985.json?start=2012-06-02T14:01:46&end=2017-06-15T15:21:40&interval=1&page=1", "data.dat");
-	GetHTTP("42.96.164.52", 80, "http://api.yeelink.net/v1.0/device/353097/sensor/397985/datapoints", "data.dat");
-	
+
+	//string str = GetHTTP("api.yeelink.net", 80, "http://api.yeelink.net/v1.0/device/353097/sensor/397985/datapoints");
+
+	double d1s1;
+	string str = YEELINK_READ::read_lastdata(353097, 397985);
+	if (!str.empty()) {
+		json jj= json::parse(str.c_str());
+		/*for (json::iterator it = j3.begin(); it != j3.end(); ++it) {
+			std::cout << it.key() << " : " << it.value() << "\n";
+		}*/
+		d1s1 = jj["value"];
+		//cout << d1s1 << endl;
+	}
+	cout << d1s1 << endl;
+
+	system("pause");
+	return 0;
 }
---
+++ Project1/Project1/yeelink_read/yeelink_read.hpp
@@ -0,0 +1,152 @@
+#ifndef __YEELINK_READ_
+#define __YEELINK_READ_
+
+#include <Winsock2.h>
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+
+class YEELINK_READ
+{
+public:
+	YEELINK_READ();
+	~YEELINK_READ();
+	static std::string read_lastdata(int device_id, int sensor_id);
+private:
+
+};
+
+YEELINK_READ::YEELINK_READ()
+{
+}
+
+YEELINK_READ::~YEELINK_READ()
+{
+}
+
+std::string YEELINK_READ::read_lastdata(int device_id, int sensor_id) {
+	std::string result;
+	std::stringstream ss, sss;
+
+	std::string sRemoteFilePath = "http://api.yeelink.net/v1.0/device/";
+	std::string device;
+	ss << device_id;
+	ss >> device;
+	sRemoteFilePath += device;
+	sRemoteFilePath += "/sensor/";
+	std::string sensor;
+	sss << sensor_id;
+	sss >> sensor;
+	sRemoteFilePath += sensor;
+	sRemoteFilePath += "/datapoints";
+	std::string sRemoteIP = "api.yeelink.net";
+	u_short nRemotePort = 80;
+
+	try
+	{
+		WORD wVersionRequested = MAKEWORD(1, 1);
+		WSADATA wsaData;
+		int nRet;
+		nRet = WSAStartup(wVersionRequested, &wsaData);
+		if (nRet)
+		{
+			char ermsg[1024] = { 0 };
+			sprintf_s(ermsg, "download %s,WSAStartup() error,error code : %d", sRemoteFilePath, nRet);
+			throw ermsg;
+		}
+
+		if (wsaData.wVersion != wVersionRequested)
+			throw "WinSock version not supported";
+
+		SOCKET Socket;
+
+		Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (Socket == INVALID_SOCKET)
+		{
+			char ermsg[1024] = { 0 };
+			sprintf_s(ermsg, "download %s,socket() error,error code : %d", sRemoteFilePath, WSAGetLastError());
+
+			throw ermsg;
+		}
+
+		// Find the port number for the HTTP service on TCP 
+		SOCKADDR_IN saServer;
+		saServer.sin_port = htons(nRemotePort);
+		saServer.sin_family = AF_INET;
+		saServer.sin_addr.s_addr = inet_addr(sRemoteIP.c_str());
+
+		nRet = connect(Socket, (LPSOCKADDR)&saServer, sizeof(SOCKADDR_IN));
+		if (nRet == SOCKET_ERROR)
+		{
+			HOSTENT* pHS = gethostbyname(sRemoteIP.c_str());
+			if (pHS != NULL)
+			{
+				in_addr addr;
+				CopyMemory(&addr.S_un.S_addr, pHS->h_addr_list[0], pHS->h_length);
+				saServer.sin_addr.S_un.S_addr = addr.S_un.S_addr;
+			}
+
+			nRet = connect(Socket, (LPSOCKADDR)&saServer, sizeof(SOCKADDR_IN));
+
+			if (nRet == SOCKET_ERROR) {
+				char ermsg[1024] = { 0 };
+				sprintf_s(ermsg, "download %s,connect() error,error code : %d", sRemoteFilePath, WSAGetLastError());
+				closesocket(Socket);
+				throw ermsg;
+			}
+		}
+
+		// Format the HTTP request 
+		char szBuffer[102400];
+
+		sprintf_s(szBuffer, "GET %s\n", sRemoteFilePath.c_str());
+		nRet = send(Socket, szBuffer, strlen(szBuffer), 0);
+		if (nRet == SOCKET_ERROR)
+		{
+			char ermsg[1024] = { 0 };
+			sprintf_s(ermsg, "download %s,send() error,error code : %d", sRemoteFilePath, WSAGetLastError());
+			closesocket(Socket);
+			throw ermsg;
+		}
+
+
+		while (1)
+		{
+			char szBuffer[102400];
+			memset(szBuffer, 0, sizeof(szBuffer));
+			nRet = recv(Socket, szBuffer, sizeof(szBuffer), 0);
+			if (nRet == SOCKET_ERROR)
+			{
+				char ermsg[1024] = { 0 };
+				sprintf_s(ermsg, "download %s,recv() error,error code : %d", sRemoteFilePath, WSAGetLastError());
+				closesocket(Socket);
+				throw ermsg;
+			}
+
+			if (nRet == 0)
+				break;
+
+			result += szBuffer;
+		}
+
+		closesocket(Socket);
+		WSACleanup();
+	}
+	catch (char* sErrMsg)
+	{
+		printf("error\n");
+
+		WSACleanup();
+	}
+
+	return result;
+
+}
+
+
+
+#endif // !__YEELINK_READ_
+
+
+
