c50ad3bdcf2215fc57c724db896d40df20835189
张胜东 <435878393@qq.com>
2017-01-24 21:05:45 +0800

    测试一下在树莓派上的效果

--- sensor/raspberrypi/dashboard.py
+++ sensor/raspberrypi/dashboard.py
@@ -1,35 +1,53 @@
 import pygame
 import color
+import math
 
 if ~pygame.font.get_init():
     pygame.font.init()
 
 
 class DASHBOARD(pygame.sprite.Sprite):
-    def __init__(self, surface, size, position, radius, index):
+    def __init__(self, surface, position, radius, index):
         pygame.sprite.Sprite.__init__(self)
         self.surface = surface
-        self.size = size
         self.radius = radius
-        self.line_width = self.radius / 45
+        self.line_width = int(math.ceil(self.radius / 45))
+        self.gap = int(math.ceil(self.line_width / 5.0))
+        self.arc_width = int(math.ceil(self.line_width * 5))
         self.position = position
         self.index = index
         self.font = pygame.font.Font(None, 36)
         self.text_index = self.font.render(str(self.index), True, color.red)
+        self.rect = (self.position[0] - self.radius, self.position[1] - self.radius, self.position[0] + self.radius,
+                     self.position[1] + self.radius)
+
 
     def draw(self):
-        pygame.draw.circle(self.surface, color.grey, self.position, self.radius, self.line_width)
+        #pygame.draw.circle(self.surface, color.grey, self.position, self.radius, self.line_width)
         self.surface.blit(self.text_index, self.position)
-        pygame.draw.line(self.surface, color.red, (self.position[0] - self.radius, self.position[1]),
-                         (self.position[0] + self.radius, self.position[1]), 2)
-        pygame.draw.line(self.surface, color.red, (self.position[0], self.position[1] - self.radius),
-                         (self.position[0], self.position[1] + self.radius), 2)
+        pygame.draw.aaline(self.surface, color.red, (self.position[0] - self.radius, self.position[1]),
+                           (self.position[0] + self.radius, self.position[1]), 2)
+        pygame.draw.aaline(self.surface, color.red, (self.position[0], self.position[1] - self.radius),
+                           (self.position[0], self.position[1] + self.radius), 2)
+        #self.draw_blendcolor_arc(color.green, color.red, 0, 180)
+        if self.index==1:
+            pygame.draw.rect(self.surface,color.red,self.rect,4)
+            print self.rect,self.radius*2
 
-        # def draw_blendcolor_arc(self,color_start,color_end,rect,angle_start,angle_stop):
-        #   pygame
-
-    def change_size(self, size):
-        self.radius = self.radius * min(size[0] / self.size[0], size[1] / self.size[1])
-        self.line_width = self.radius / 45
-        self.position = (self.position[0] * size[0] / self.size[0], self.position[1] * size[1] / self.size[1])
-        self.size = size
+    def draw_blendcolor_arc(self, color_start, color_end, degree_start, degree_stop):
+        degree_start = int(degree_start)
+        degree_stop = int(degree_stop)
+        color_i = color_start
+        rect = (self.rect[0] + self.line_width + self.gap, self.rect[1] + self.line_width + self.gap,
+                self.rect[2] - self.line_width - self.gap, self.rect[3] - self.line_width - self.gap)
+        print rect
+        for degree in range(degree_start, degree_stop + 1):
+            color_i.r = color_i.r + (color_end.r - color_start.r) / (degree_stop - degree_start) * (
+                degree - degree_start)
+            color_i.g = color_i.g + (color_end.g - color_start.g) / (degree_stop - degree_start) * (
+                degree - degree_start)
+            color_i.b = color_i.b + (color_end.b - color_start.b) / (degree_stop - degree_start) * (
+                degree - degree_start)
+            pygame.draw.arc(self.surface, color.green, rect, math.radians(degree), math.radians(degree), 10)
+        pygame.draw.rect(self.surface, color.blue, rect, 5)
+        pygame.draw.circle(self.surface,color.red,self.position,self.radius-self.line_width-self.gap,1)
--- sensor/raspberrypi/test_pygame.py
+++ sensor/raspberrypi/test_pygame.py
@@ -13,6 +13,8 @@ gap = (int(size[0] / 14), int(size[1] / 9))
 radius = min(gap[0], gap[1]) * 2
 size = (2 * gap[0] + 6 * radius, gap[1] + 4 * radius)
 
+print size
+
 screen = pygame.display.set_mode(size, FULLSCREEN | HWSURFACE)
 
 x_mydashboard = 3
@@ -22,7 +24,7 @@ for y in range(0, y_mydashboard):
     for x in range(0, x_mydashboard):
         position = (radius + x * (gap[0] + 2 * radius), radius + y * (gap[1] + 2 * radius))
         print position, radius
-        mydashboard.append(dashboard.DASHBOARD(screen, size, position, radius, x + y * x_mydashboard))
+        mydashboard.append(dashboard.DASHBOARD(screen, position, radius, x + y * x_mydashboard))
 
 while True:
     for event in pygame.event.get():
@@ -37,7 +39,7 @@ while True:
     for i in range(0, x_mydashboard * y_mydashboard):
         mydashboard[i].draw()
 
-    pygame.draw.rect(screen, color.red, (0, 0, size[0], size[1]), 10)
-
+    #pygame.draw.rect(screen, color.red, (0, 0, size[0], size[1]), 10)
+    #pygame.draw.rect(screen,color.red,(100,100,300,300),2)
     pygame.display.flip()
-    pygame.time.delay(10)
+    pygame.time.delay(2000)
