b3fadba72921829cc559ad5c90e0ccbc3353e044
张胜东 <435878393@qq.com>
2017-01-24 21:12:50 +0800

    增加了忽略文件

--- sensor/raspberrypi/dashboard.py
+++ sensor/raspberrypi/dashboard.py
@@ -21,18 +21,17 @@ class DASHBOARD(pygame.sprite.Sprite):
         self.rect = (self.position[0] - self.radius, self.position[1] - self.radius, self.position[0] + self.radius,
                      self.position[1] + self.radius)
 
-
     def draw(self):
-        #pygame.draw.circle(self.surface, color.grey, self.position, self.radius, self.line_width)
+        # pygame.draw.circle(self.surface, color.grey, self.position, self.radius, self.line_width)
         self.surface.blit(self.text_index, self.position)
         pygame.draw.aaline(self.surface, color.red, (self.position[0] - self.radius, self.position[1]),
                            (self.position[0] + self.radius, self.position[1]), 2)
         pygame.draw.aaline(self.surface, color.red, (self.position[0], self.position[1] - self.radius),
                            (self.position[0], self.position[1] + self.radius), 2)
-        #self.draw_blendcolor_arc(color.green, color.red, 0, 180)
-        if self.index==1:
-            pygame.draw.rect(self.surface,color.red,self.rect,4)
-            print self.rect,self.radius*2
+        # self.draw_blendcolor_arc(color.green, color.red, 0, 180)
+        if self.index == 1:
+            pygame.draw.rect(self.surface, color.red, self.rect, 4)
+            print self.rect, self.radius * 2
 
     def draw_blendcolor_arc(self, color_start, color_end, degree_start, degree_stop):
         degree_start = int(degree_start)
@@ -50,4 +49,4 @@ class DASHBOARD(pygame.sprite.Sprite):
                 degree - degree_start)
             pygame.draw.arc(self.surface, color.green, rect, math.radians(degree), math.radians(degree), 10)
         pygame.draw.rect(self.surface, color.blue, rect, 5)
-        pygame.draw.circle(self.surface,color.red,self.position,self.radius-self.line_width-self.gap,1)
+        pygame.draw.circle(self.surface, color.red, self.position, self.radius - self.line_width - self.gap, 1)
