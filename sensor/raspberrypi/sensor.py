def48dbea4796fb183c895fcee0211384aa7ba1f
张胜东 <435878393@qq.com>
2017-01-29 21:49:09 +0800

    测试

--- sensor/raspberrypi/sensor.py
+++ sensor/raspberrypi/sensor.py
@@ -7,25 +7,29 @@ _DEBUG_ = False
 
 MYID = 1
 yeelink_config.init(MYID)
+device_id = yeelink_config.device_id()
 
 while True:
     value_pm25 = sensor_api.read_pm25()
     if value_pm25 > 0:
         print "pm2.5 :", value_pm25
         if not _DEBUG_:
-            yeelink_api.send_value(yeelink_config.device_id(), yeelink_config.sensor_pm25_id(), value_pm25)
+            sensor_pm25_id = yeelink_config.sensor_pm25_id()
+            yeelink_api.send_value(device_id, sensor_pm25_id, value_pm25)
 
     value_CO = sensor_api.read_CO()
     if value_CO > 0:
         print "CO :", value_CO
         if not _DEBUG_:
-            yeelink_api.send_value(yeelink_config.device_id(), yeelink_config.sensor_CO_id(), value_CO)
+            sensor_CO_id = yeelink_config.sensor_CO_id()
+            yeelink_api.send_value(device_id, sensor_CO_id, value_CO)
 
     value_SO2 = sensor_api.read_SO2()
     if value_SO2 > 0:
         print "SO2 :", value_SO2
         if not _DEBUG_:
-            yeelink_api.send_value(yeelink_config.device_id(), yeelink_config.sensor_SO2_id(), value_SO2)
+            sensor_SO2_id = yeelink_config.sensor_SO2_id()
+            yeelink_api.send_value(device_id, sensor_SO2_id, value_SO2)
 
     # print "sleep"
     time.sleep(15)
--- sensor/raspberrypi/yeelink_api.py
+++ sensor/raspberrypi/yeelink_api.py
@@ -12,13 +12,14 @@ def init(apikey):
 def send_value(device_id, sensor_id, value):
     mycurl = pycurl.Curl()
     mycurl.setopt(mycurl.URL,
-                  'http://api.yeelink.net/v1.0/device/' + str(device_id) + '/sensor/' + str(sensor_id) + '/datapoints')
+                  'http://api.yeelink.net/v1.0/device/' + device_id + '/sensor/' + sensor_id + '/datapoints')
     mycurl.setopt(mycurl.HTTPHEADER, ["U-ApiKey:" + apikey])
     mycurl.setopt(mycurl.POSTFIELDS, json.dumps({"value": value}))
     try:
-        print 'http://api.yeelink.net/v1.0/device/' + str(device_id) + '/sensor/' + str(sensor_id) + '/datapoints'
+        print 'http://api.yeelink.net/v1.0/device/' + device_id + '/sensor/' + sensor_id + '/datapoints'
         print "mycurl perform start : yeelink_api send_value"
         mycurl.perform()
+        print ""
         print "mycurl perform end : yeelink_api send_value"
     except Exception, e:
         # print Exception, ":", e
