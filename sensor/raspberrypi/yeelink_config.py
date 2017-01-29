88b956dd6c11068c4f2d9ab19eb705492b5fa65a
张胜东 <435878393@qq.com>
2017-01-29 20:42:08 +0800

    优化结构

--- sensor/raspberrypi/yeelink_config.py
+++ sensor/raspberrypi/yeelink_config.py
@@ -2,16 +2,29 @@ apikey = "779bfd896876dc377d3ed78d0fa1dbf4"
 
 
 def init(myid):
-    self.MYID = myid
+    if myid == 0:
+        self.device_id = '353097'
+        self.sensor_pm25_id = '397985'
+        self.sensor_CO_id = '398391'
+        self.sensor_SO2_id = '400110'
+    elif myid == 1:
+        self.device_id = '354298'
+        self.sensor_pm25_id = '400108'
+        self.sensor_CO_id = '400109'
+        self.sensor_SO2_id = '400111'
 
 
-if MYID == 0:
-    device_id = '353097'
-    sensor_pm25_id = '397985'
-    sensor_CO_id = '398391'
-    sensor_SO2_id = '400110'
-elif MYID == 1:
-    device_id = '354298'
-    sensor_pm25_id = '400108'
-    sensor_CO_id = '400109'
-    sensor_SO2_id = '400111'
+def device_id():
+    return self.device_id
+
+
+def sensor_pm25_id():
+    return self.sensor_pm25_id
+
+
+def sensor_CO_id():
+    return self.sensor_CO_id
+
+
+def sensor_SO2_id():
+    return self.sensor_SO2_id
