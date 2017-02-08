d497eada432864bf59cc47bebe13d032b0b32b0d
张胜东 <435878393@qq.com>
2017-02-09 04:01:59 +0800

    优化了curl和json写法

--- server/weixin/index.php
+++ server/weixin/index.php
@@ -1,27 +1,27 @@
 <?php
 /**
-  * wechat php test
-  */
+ * wechat php test
+ */
 
 //define your token
 define("TOKEN", "weixin");
 $wechatObj = new wechatCallbackapiTest();
-if (!isset($_GET['echostr'])){
-	$wechatObj->responseMsg();
-}else{
-	$wechatObj->valid();
+if (!isset($_GET['echostr'])) {
+    $wechatObj->responseMsg();
+} else {
+    $wechatObj->valid();
 }
 
 class wechatCallbackapiTest
 {
-	public function valid()
+    public function valid()
     {
         $echoStr = $_GET["echostr"];
 
         //valid signature , option
-        if($this->checkSignature()){
-        	echo $echoStr;
-        	exit;
+        if ($this->checkSignature()) {
+            echo $echoStr;
+            exit;
         }
     }
 
@@ -30,87 +30,93 @@ class wechatCallbackapiTest
      */
     public function responseMsg()
     {
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-      	//extract post data
-		if (!empty($postStr)){
-                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-                   the best way is to check the validity of xml by yourself */
-                libxml_disable_entity_loader(true);
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
+        //extract post data
+        if (!empty($postStr)) {
+            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+               the best way is to check the validity of xml by yourself */
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
 							<FromUserName><![CDATA[%s]]></FromUserName>
 							<CreateTime>%s</CreateTime>
 							<MsgType><![CDATA[%s]]></MsgType>
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
-							</xml>";             
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
+							</xml>";
+            if (!empty($keyword)) {
+                $msgType = "text";
 
-					$device_id=353097;
-					$sensor_id=397985;
-					$data=file_get_contents('http://api.yeelink.net/v1.0/device/'.$device_id.'/sensor/'.$sensor_id.'/datapoints');
-                    //$data_json=json_decode($data,true);
-                    //$contentStr='传感器报警！当前数值为 : '.$data_json["value"];
-                    $data_json=json_decode($data);
-                    $contentStr='传感器报警！当前数值为 : '.$data_json->value;
-                    
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
+                $device_id = 353097;
+                $sensor_id = 397985;
+                $durl = "http://api.yeelink.net/v1.0/device/$device_id/sensor/$sensor_id/datapoints";
+                //$data = file_get_contents($durl);
+                //$data_json=json_decode($data,true);
+                //$contentStr='传感器报警！当前数值为 : '.$data_json["value"];
+                //$data_json = json_decode($data);
+                //$r = $this->curl_file_get_contents($durl);
+                //$contentStr = "传感器报警！当前数值为 : $data_json->value \n\n测试curl中:\n$r";
+                $data = $this->curl_file_get_contents($durl);
+                $data_json = json_decode($data, true);
+                $value = $data_json["value"];
+                $contentStr = "传感器报警！当前数值为 : $value \n\n测试curl中:\n$data";
 
-        }else {
-        	echo "lalala";
-        	exit;
+
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            } else {
+                echo "Input something...";
+            }
+
+        } else {
+            echo "lalala";
+            exit;
         }
     }
-		
-	private function checkSignature()
-	{
+
+    private function checkSignature()
+    {
         // you must define TOKEN by yourself
         if (!defined("TOKEN")) {
             throw new Exception('TOKEN is not defined!');
         }
-        
+
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-        		
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
         // use SORT_STRING rule
-		sort($tmpArr, SORT_STRING);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
-		
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
-/*
-	function curl_file_get_contents($durl){  
-    	$ch = curl_init();  
-    	curl_setopt($ch, CURLOPT_URL, $durl);  
-    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 获取数据返回    
-    	curl_setopt($ch, CURLOPT_BINARYTRANSFER, true); // 在启用 CURLOPT_RETURNTRANSFER 时候将获取数据返回    
-    	$r = curl_exec($ch);  
-    	curl_close($ch);  
-    	return $r;  
-	}
-*/
-	
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function curl_file_get_contents($durl)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $durl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//获取数据返回
+        $r = curl_exec($ch);
+        curl_close($ch);
+        return $r;
+    }
+
+
 }
 
 ?>
--- server/weixin/test.php
+++ server/weixin/test.php
@@ -5,11 +5,12 @@
  * Date: 2017/2/9,0009
  * Time: 01:17
  */
-$device_id='353097';
-$sensor_id='397985';
-$data=file_get_contents('http://api.yeelink.net/v1.0/device/'.$device_id.'/sensor/'.$sensor_id.'/datapoints');
-$data_json=json_decode($data);
-$contentStr='value : '.$data_json->value;
+$device_id = 353097;
+$sensor_id = 397985;
+$durl = "http://api.yeelink.net/v1.0/device/$device_id/sensor/$sensor_id/datapoints";
+$data = file_get_contents($durl);
+$data_json = json_decode($data);
+$contentStr = 'value : ' . $data_json->value;
 echo $contentStr
 
 ?>
