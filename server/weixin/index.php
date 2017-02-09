77a549327da6d91dc9f2962fa5ebd3ade71895c5
张胜东 <435878393@qq.com>
2017-02-10 05:55:44 +0800

    增加了发送模板消息的功能

--- server/weixin/index.php
+++ server/weixin/index.php
@@ -66,17 +66,22 @@ class wechatCallbackapiTest
                 //$data_json = json_decode($data);
                 //$r = $this->curl_file_get_contents($durl);
                 //$contentStr = "传感器报警！当前数值为 : $data_json->value \n\n测试curl中:\n$r";
-                $data = $this->curl_get_contents($durl);
+                $data = $this->curl_request($durl);
                 $data_json = json_decode($data, true);
                 $value = $data_json["value"];
-                $contentStr = "传感器报警！当前数值为 : $value \n\n测试curl中:\n$data";
+                $contentStr = "传感器报警！当前数值为 : $value";
                 if (time() > $this->time_expires_in) {
                     $this->update_access_token();
                 }
-                $contentStr .= " \n\naccess_token:$this->access_token";
-
+                //$contentStr .= "\n\naccess_token:$this->access_token";
+                $template = array('touser' => "$fromUsername", 'template_id' => "Oh5bDFWIIdg8acICj639FGPeLNMNxP0X68uWykjZLuM", 'url' => "http://github.com/zhangsheng377", 'data' => array('first' => array('value' => urlencode("传感器报警！"), 'color' => "#743A3A"), 'second' => array('value' => urlencode("$value"), 'color' => "#FF0000")));
+                $data_template = $this->curl_request("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=$this->access_token", urldecode(json_encode($template)));
+                $contentStr .= "\n\ntemplate:$data_template";
+                //$contentStr .= "\n\nfromUsername:$fromUsername\ntoUsername:$toUsername";
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                 echo $resultStr;
+
+
             } else {
                 echo "Input something...";
             }
@@ -112,16 +117,19 @@ class wechatCallbackapiTest
         }
     }
 
-    private function curl_get_contents($durl)
+    private function curl_request($durl, $data = null)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $durl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//获取数据返回
+        if (!empty($data)) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
         $r = curl_exec($ch);
         curl_close($ch);
         return $r;
     }
-
 
     private function update_access_token()
     {
@@ -135,7 +143,7 @@ class wechatCallbackapiTest
         } else {
             $appid = "wx691945ff03ba6040";
             $appsecret = "1e22eb6471847adef6c330719a739773";
-            $data = $this->curl_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$appsecret");
+            $data = $this->curl_request("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$appsecret");
             $data_json = json_decode($data, true);
             $this->access_token = $data_json["access_token"];
             $this->time_expires_in = time() + $data_json["expires_in"] - 200;
