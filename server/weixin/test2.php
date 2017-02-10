5b0045c0df419cdb072f091f6e2051a9129f54b4
张胜东 <435878393@qq.com>
2017-02-11 04:41:25 +0800

    证明了的确关不了安全模式

--- server/weixin/test2.php
+++ server/weixin/test2.php
@@ -8,4 +8,18 @@
 
 echo "hello\n";
 
+$data_return=ini_set('safe_mode','Off');
+$data_return_0 = ignore_user_abort(true);
+$data_return_1 = ini_set('max_execution_time', '0');
+$data_return_2 = set_time_limit(0);
+$file_write = fopen("data_return.dat", "wb");
+fwrite($file_write, $data_return);
+fwrite($file_write, "\n");
+fwrite($file_write, $data_return_0);
+fwrite($file_write, "\n");
+fwrite($file_write, $data_return_1);
+fwrite($file_write, "\n");
+fwrite($file_write, $data_return_2);
+fclose($file_write);
+
 ?>
