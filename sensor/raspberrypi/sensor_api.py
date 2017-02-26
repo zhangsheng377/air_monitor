e20228f4d1115aba5c4d756d111272668c9f39c6
张胜东 <435878393@qq.com>
2017-02-26 21:53:38 +0800

    改回去

--- sensor/raspberrypi/sensor_api.py
+++ sensor/raspberrypi/sensor_api.py
@@ -10,14 +10,10 @@ class SENSOR_CONFIG():
     A = 1200
 
     address = 0x48
-    # A0 = 0x40
-    # A1 = 0x41
-    # A2 = 0x42
-    # A3 = 0x43
-    A0 = 0x41
-    A1 = 0x42
-    A2 = 0x43
-    A3 = 0x44
+    A0 = 0x40
+    A1 = 0x41
+    A2 = 0x42
+    A3 = 0x43
 
     bus = smbus.SMBus(1)
 
