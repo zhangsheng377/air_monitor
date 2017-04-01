575d0b911d7c7528fcfcacd286d704db55849e22
张胜东 <435878393@qq.com>
2017-04-01 22:29:43 +0800

    改为arduino向树莓派汇报

--- sensor/arduino/sensor/sensor.ino
+++ sensor/arduino/sensor/sensor.ino
@@ -1,97 +1,172 @@
 #include <SoftwareSerial.h>
 
-#define CO A0
-#define NONE A1
-
-float A = 1000.0;
 #define pm25_RX 10
 #define pm25_TX 11
+#define CO A0
+#define SO2 A1
+#define O3 A2
+
+SoftwareSerial mySerial_pm(pm25_RX, pm25_TX);
+float A = 1000.0;
+
+float value_pm25 = 0.0;
+float value_CO = 0.0;
+float value_SO2 = 0.0;
+float value_O3 = 0.0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  mySerial_pm.begin(2400);
   pinMode(CO, INPUT);
-  pinMode(NONE, INPUT);
+  pinMode(SO2, INPUT);
+  pinMode(O3, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println("start");
-  float value_pm25 = read_pm(pm25_RX, pm25_TX);
-  Serial.print("pm2.5 : "); Serial.print(value_pm25); Serial.println("ppm");
+  int temp;
+  temp = read_pm(pm25_RX, pm25_TX);
+  if (temp > 0) {
+    value_pm25 = temp;
+  }
+  Serial.print("pm2.5:"); Serial.println(value_pm25);
 
-  float value_CO = map(analogRead(CO) - analogRead(NONE), 0, 1023, 0, 1000);
-  Serial.print("CO : "); Serial.print(value_CO); Serial.println("ppm");
+  value_CO = map(analogRead(CO), 0, 1023, 0, 1000);
+  Serial.print("CO:"); Serial.println(value_CO);
 
-  Serial.println("");
-  delay(500);
+  value_SO2 = map(analogRead(SO2), 0, 1023, 0, 500);
+  Serial.print("SO2:"); Serial.println(value_SO2);
+
+  value_O3 = map(analogRead(O3), 0, 1023, 0, 1000);
+  Serial.print("O3:"); Serial.println(value_O3);
+
+ //Serial.println("");
+  //delay(500);
 }
 
 float read_pm(int RX, int TX) {
-  SoftwareSerial mySerial_pm(RX, TX);
-  mySerial_pm.begin(2400);
   int Vout_H, Vout_L, Vret_H, Vret_L, check, temp;
   float Vout, Ud = 0.0;
-  mySerial_pm.flush();
-  //Serial.println("read_pm");
-  while (true) {
-    temp = -1;
-    while (temp == -1) {
-      temp = mySerial_pm.read();
-      //Serial.println(temp);
+
+
+  int count = 0;
+  int need_read = mySerial_pm.available();
+  //Serial.print("need_read : "); Serial.println(need_read);
+  for (int i = 0; i < need_read; i++) {
+    temp = mySerial_pm.read();
+    if (temp == -1) {
+      continue;
     }
-    //Serial.println(temp);
-    if (temp == 170) {
-      //Serial.println(temp);
-      temp = -1;
-      while (temp == -1) {
-        temp = mySerial_pm.read();
+    //Serial.print(temp);
+    if (count == 0) {
+      if (temp != 170) {
+        continue;
       }
+      count++;
+    }
+    else if (count == 1) {
       Vout_H = temp;
-
-      temp = -1;
-      while (temp == -1) {
-        temp = mySerial_pm.read();
-      }
+      count++;
+    }
+    else if (count == 2) {
       Vout_L = temp;
-
-      temp = -1;
-      while (temp == -1) {
-        temp = mySerial_pm.read();
-      }
+      count++;
+    }
+    else if (count == 3) {
       Vret_H = temp;
-
-      temp = -1;
-      while (temp == -1) {
-        temp = mySerial_pm.read();
-      }
+      count++;
+    }
+    else if (count == 4) {
       Vret_L = temp;
-
-      temp = -1;
-      while (temp == -1) {
-        temp = mySerial_pm.read();
-      }
+      count++;
+    }
+    else if (count == 5) {
       check = temp;
-
-      temp = -1;
-      while (temp == -1) {
-        temp = mySerial_pm.read();
-      }
+      count++;
+    }
+    else if (count == 6) {
       if (temp == 255) {
-        //Serial.print(Vout_H);Serial.print(" ");Serial.print(Vout_L);Serial.print(" ");Serial.print(Vret_H);Serial.print(" ");Serial.print(Vret_L);Serial.print(" ");Serial.println(check);
         if (check == (Vout_H + Vout_L + Vret_H + Vret_L) % 256) {
           Vout = (Vout_H * 256 + Vout_L) * 1.0 / 1024 * 8;
           Ud = 1.0 * A * Vout;
-          //Serial.println(Ud);
           if (Ud > 0) {
-            //Serial.println("Ud>0");
-            //Serial.print(Vout_H);Serial.print(" ");Serial.print(Vout_L);Serial.print(" ");Serial.print(Vret_H);Serial.print(" ");Serial.print(Vret_L);Serial.print(" ");Serial.println(check);
             break;
+          }
+          else {
+            count = 0;
           }
         }
       }
-      temp = -1;
     }
   }
   return Ud;
+
+  /*
+    SoftwareSerial mySerial_pm(RX, TX);
+    mySerial_pm.begin(2400);
+    //mySerial_pm.flush();
+    //Serial.println("read_pm");
+    while (true) {
+      temp = -1;
+      while (temp == -1) {
+        temp = mySerial_pm.read();
+        Serial.print(temp);
+      }
+      Serial.println(temp);
+      if (temp == 170) {
+        //Serial.println(temp);
+        temp = -1;
+        while (temp == -1) {
+          temp = mySerial_pm.read();
+        }
+        Vout_H = temp;
+
+        temp = -1;
+        while (temp == -1) {
+          temp = mySerial_pm.read();
+        }
+        Vout_L = temp;
+
+        temp = -1;
+        while (temp == -1) {
+          temp = mySerial_pm.read();
+        }
+        Vret_H = temp;
+
+        temp = -1;
+        while (temp == -1) {
+          temp = mySerial_pm.read();
+        }
+        Vret_L = temp;
+
+        temp = -1;
+        while (temp == -1) {
+          temp = mySerial_pm.read();
+        }
+        check = temp;
+
+        temp = -1;
+        while (temp == -1) {
+          temp = mySerial_pm.read();
+        }
+        if (temp == 255) {
+          //Serial.print(Vout_H);Serial.print(" ");Serial.print(Vout_L);Serial.print(" ");Serial.print(Vret_H);Serial.print(" ");Serial.print(Vret_L);Serial.print(" ");Serial.println(check);
+          if (check == (Vout_H + Vout_L + Vret_H + Vret_L) % 256) {
+            Vout = (Vout_H * 256 + Vout_L) * 1.0 / 1024 * 8;
+            Ud = 1.0 * A * Vout;
+            //Serial.println(Ud);
+            if (Ud > 0) {
+              //Serial.println("Ud>0");
+              //Serial.print(Vout_H);Serial.print(" ");Serial.print(Vout_L);Serial.print(" ");Serial.print(Vret_H);Serial.print(" ");Serial.print(Vret_L);Serial.print(" ");Serial.println(check);
+              break;
+            }
+          }
+        }
+        temp = -1;
+      }
+    }
+    return Ud;
+  */
 }
---
+++ sensor/raspberrypi/test.py
@@ -0,0 +1,17 @@
+import serial
+import time
+
+serial_0 = serial.Serial("/dev/ttyUSB0", 9600)
+
+while True:
+    waitlen = serial_0.inWaiting()
+    if waitlen != 0:
+        recv = serial_0.read(waitlen)
+        recv_split=recv.split('\n')
+        for message in recv_split:
+            message_split=message.split(':')
+            name=message_split[0]
+            value_str=message_split[1]
+            value=float(value_str)
+            print name,"--",value
+    time.sleep(0.1)
