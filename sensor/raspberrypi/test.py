57f07c9b1800949bbdf8a05522bbe2d4f80ccd0c
张胜东 <435878393@qq.com>
2017-04-01 22:37:29 +0800

    增加了数组大小的检查

--- sensor/raspberrypi/test.py
+++ sensor/raspberrypi/test.py
@@ -7,11 +7,12 @@ while True:
     waitlen = serial_0.inWaiting()
     if waitlen != 0:
         recv = serial_0.read(waitlen)
-        recv_split=recv.split('\n')
+        recv_split = recv.split('\n')
         for message in recv_split:
-            message_split=message.split(':')
-            name=message_split[0]
-            value_str=message_split[1]
-            value=float(value_str)
-            print name,"--",value
+            message_split = message.split(':')
+            if len(message_split) > 1:
+                name = message_split[0]
+                value_str = message_split[1]
+                value = float(value_str)
+                print name, "--", value
     time.sleep(0.1)
