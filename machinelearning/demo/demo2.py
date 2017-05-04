7567ea7c68b8fe8760cc2c1c1384e04c1082c002
张胜东 <435878393@qq.com>
2017-05-04 13:18:50 +0800

    机器学习的预测模块已经做好~~采取在线学习+线性回归~

--- machinelearning/demo/demo2.py
+++ machinelearning/demo/demo2.py
@@ -32,7 +32,11 @@ def best_k(xArr, yArr):
     return k
 
 
-mothed = 0
+start_count = 10
+sum_rate = 0.0
+count = 0
+
+mothed = 1
 if mothed == 0:
     xArr, yArr = regression.loadDataSet("foo.txt")
     k = best_k(xArr, yArr)
@@ -46,30 +50,44 @@ else:
     m = (shape(xArr_origin))[0]
     yHat = zeros(m)
 
-    xArr.append(xArr_origin[1][:])
-    xArr[0] = xArr[0] + xArr_origin[0][:]
-    yArr.append(yArr_origin[1])
-    yHat[0] = yArr_origin[1]
-    for i in range(2, m - 3):
-        k = best_k(xArr, yArr)
-        x = xArr_origin[i][:] + xArr_origin[i - 1][:]
+    xArr.append(xArr_origin[0][:])
+    yArr.append(yArr_origin[0])
+    yHat[0] = yArr_origin[0]
+
+    for i in range(1, m):
+        # k = best_k(xArr, yArr)
+        k = 0.06830078125
+        x = xArr_origin[i][:]
         y = regression.lwlr(x, xArr, yArr, k)
         # print(y.flatten().A[0][0])
-        yHat[i - 1] = y.flatten().A[0][0]
+        yHat[i] = y.flatten().A[0][0]
         while yHat[i] <= 0:
             k = k + 0.01
             y = regression.lwlr(x, xArr, yArr, k)
             yHat[i] = y.flatten().A[0][0]
         xArr.append(x)
         yArr.append(yArr_origin[i])
-        print(k, yHat[i - 1], yArr[i - 1])
-        # print(yArr)
-
-# k = best_k(xArr, yArr)
+        print(i, k, yHat[i], yArr[i])
+        with open('workfile', 'a') as f:
+            f.write(str(i))
+            f.write("\t")
+            f.write(str(k))
+            f.write("\t")
+            f.write(str(yHat[i]))
+            f.write("\t")
+            f.write(str(yArr[i]))
+            f.write("\t")
+            rate = abs(yHat[i] - yArr[i]) * 1.0 / yArr[i]
+            f.write(str(rate))
+            f.write("\n")
+        if i > sum_rate:
+            sum_rate += rate
+            count += 1
+            # print(yArr)  # k = best_k(xArr, yArr)
 # yHat = regression.lwlrTest(xArr, xArr, yArr, k)
-print(yHat)
+# print(yHat)
 # print(regression.rssError(yArr, yHat))
-
+print(sum_rate * 1.0 / count)
 # print(yArr)
 # print(yHat)
 xMat = mat(xArr)
--- machinelearning/demo/regression.py
+++ machinelearning/demo/regression.py
@@ -2,13 +2,13 @@ from numpy import *
 
 
 def loadDataSet(fileName):
-    numFeat = len(open(fileName).readline().split(' ')) - 1
+    numFeat = len(open(fileName).readline().split('\t')) - 1
     dataMat = []
     labelMat = []
     fr = open(fileName)
     for line in fr.readlines():
         lineArr = []
-        curLine = line.strip().split(' ')
+        curLine = line.strip().split('\t')
         for i in range(numFeat):
             lineArr.append(float(curLine[i]))
         dataMat.append(lineArr)
