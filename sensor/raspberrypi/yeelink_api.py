afbccb0039ea0c1f4765663cd50468226592786a
zhangsheng377 <435878393@qq.com>
2017-08-22 09:34:59 +0800

    use multi-thread to send socket

--- sensor/raspberrypi/yeelink_api.py
+++ sensor/raspberrypi/yeelink_api.py
@@ -1,10 +1,11 @@
 import pycurl
 import json
+import thread
 
 
 # import StringIO
 
-def send_value(apikey, device_id, sensor_id, value):
+def send_value_raw(apikey, device_id, sensor_id, value):
     # print device_id, sensor_id, value
     mycurl = pycurl.Curl()
     mycurl.setopt(mycurl.URL,
@@ -17,3 +18,7 @@ def send_value(apikey, device_id, sensor_id, value):
         print Exception, ":", e
         # print ""
     mycurl.close()
+    # thread.exit_thread()
+
+def send_value(apikey, device_id, sensor_id, value):
+    thread.start_new_thread(send_value_raw,(apikey, device_id, sensor_id, value))
